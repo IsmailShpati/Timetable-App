622fca659ac947670d4c736b8c8fca6b5dbf0bd8
IsmailShpati <75791043+IsmailShpati@users.noreply.github.com>
2021-03-01 18:59:34 +0100

    Activity class This class will contain each daily activity.

---
+++ app/src/main/java/com/example/timetableapp/model/Activity.java
@@ -0,0 +1,64 @@
+package com.example.timetableapp.model;
+
+public class Activity {
+    private String name, description;
+    private Time startTime, endTime;
+    private int repeatingDay;
+
+    public Activity(){
+    }
+
+    public Activity(String name, Time startTime, Time endTime){
+        this.name = name;
+        this.startTime = startTime;
+        this.endTime = endTime;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Time getStartTime(){
+        return startTime;
+    }
+
+    public void setStartTime(Time startTime){
+        this.startTime = startTime;
+    }
+
+    public void setStartTime(int hour, int minute){
+        this.startTime = new Time(hour, minute);
+    }
+
+    public Time getEndTime(){
+        return endTime;
+    }
+
+    public void setEndTime(Time endTime){
+        this.endTime = endTime;
+    }
+
+    public void setEndTime(int hour, int minute){
+        this.endTime = new Time(hour, minute);
+    }
+
+    public int getRepeatingDay() {
+        return repeatingDay;
+    }
+
+    public void setRepeatingDay(int repeatingDay) {
+        this.repeatingDay = repeatingDay;
+    }
+}
