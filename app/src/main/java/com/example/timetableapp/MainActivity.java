8438122686d43df06881a724293685718dbc07b5
IsmailShpati <75791043+IsmailShpati@users.noreply.github.com>
2021-03-01 19:01:17 +0100

    All project files commited

---
+++ app/src/main/java/com/example/timetableapp/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.timetableapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.provider.AlarmClock;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.Calendar;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
